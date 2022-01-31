09044b8de07255aa39f1c8261e2e0c44144a5880
Danil <visloguzov500@gmail.com>
2022-01-31 21:19:46 +0300

    test

---
+++ main.py
@@ -0,0 +1,3 @@
+n = int(input("Введите число: "))
+z = n + int(str(n) + str (n)) + int(str( n ) + str(n) + str(n))
+print(f'Сумма чисел: {z}')
---
+++ main2.py
@@ -0,0 +1,6 @@
+name = input('Введите "Ваше" имя: ')
+let = input(name + ', сколько Вам лет: ')
+let = int(let)
+god = int(input(name + ', какой сейчас год: '))
+ngod = let + god
+print(name + ', в ' + str(ngod) + ' Вам будет ' + str(let * 2) + ' лет.')
